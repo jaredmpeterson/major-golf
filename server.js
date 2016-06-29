d064cb4aa715768beab9cf74f80b3a77c35e7032
Jared M. Peterson <jared@peteyinc.com>
2016-06-29 17:03:33 -0600

    node and express

--- server.js
+++ server.js
@@ -8,6 +8,13 @@ var zebras = require('./zebras.js');
 var port = 3000;
 var app = express();
 
+app.use(bodyParser.json());
+app.use(session({
+  secret: zebras.secret
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.static(__dirname + '/public'));
 
 app.listen(port, function() {
   console.log("Fore! " + port);
