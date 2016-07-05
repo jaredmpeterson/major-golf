3d9162bee741bede315e019988ea9643220b02e8
Jared M. Peterson <jared@peteyinc.com>
2016-07-05 10:56:38 -0600

    scss

--- gulpfile.js
+++ gulpfile.js
@@ -5,8 +5,6 @@ var sass = require('gulp-sass');
 var order = require('gulp-order');
 var ngAnnotate = require('gulp-ng-annotate');
 var sourcemaps = require('gulp-sourcemaps');
-// var autoprefixer = require('gulp-autoprefixer');
-// var postcss = require('gulp-postcss');
 
 gulp.task('es6', function() {
   gulp.src(['public/js/*.js'])
@@ -17,14 +15,6 @@ gulp.task('es6', function() {
   .pipe(concat('majorgolf.js'))
   .pipe(gulp.dest('public/dist/'));
 });
-
-// gulp.task('autoprefixer', function () {
-//     return gulp.src('public/styles/*.css')
-//         .pipe(sourcemaps.init())
-//         .pipe(postcss([ autoprefixer({ browsers: ['last 2 versions'] }) ]))
-//         .pipe(sourcemaps.write('.'))
-//         .pipe(gulp.dest('public/dist/'));
-// });
 
 gulp.task('sass', function() {
   return gulp.src('public/styles/*.scss')
