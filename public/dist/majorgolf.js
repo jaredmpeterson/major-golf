8cce60e69fb443b9f48de7ac0d2ea5e37a0161aa
Jared M. Peterson <jared@peteyinc.com>
2016-07-12 12:16:21 -0600

    css fixes

--- public/dist/majorgolf.js
+++ public/dist/majorgolf.js
@@ -168,6 +168,54 @@ angular.module('majorGolf').controller('gameCtrl', function ($scope, gameService
 });
 'use strict';
 
+// INITILIZE SERVICE
+// ============================================================
+angular.module('majorGolf').service('gameService', function ($http) {
+
+  // CRUD FUNCTIONS
+  // ============================================================
+  this.getGame = function (id) {
+    // var query = "";
+    // if (id) query = '?_id=' + id;
+    return $http({
+      method: 'GET',
+      url: '/api/games'
+    }).then(function (response) {
+      return response.data;
+    });
+  };
+  this.createGame = function (game) {
+    return $http({
+      method: 'POST',
+      url: '/api/games',
+      data: game
+    }).then(function (response) {
+      return response;
+    });
+  };
+  this.editGame = function (id, game) {
+    return $http({
+      method: 'PUT',
+      url: "/api/games/" + id,
+      data: game
+    }).then(function (response) {
+      return response;
+    });
+  };
+  this.deleteGame = function (id) {
+    return $http({
+      method: 'DELETE',
+      url: '/api/games/' + id
+    }).then(function (response) {
+      return response;
+    });
+  };
+
+  // OTHER FUNCTIONS
+  // ============================================================
+});
+'use strict';
+
 angular.module('majorGolf').controller('eventCtrl', function ($scope, eventService, $q) {
 
   // console.log(events);
@@ -253,54 +301,6 @@ angular.module('majorGolf').service('eventService', function ($http) {
     return $http({
       method: 'DELETE',
       url: '/api/event/' + id
-    }).then(function (response) {
-      return response;
-    });
-  };
-
-  // OTHER FUNCTIONS
-  // ============================================================
-});
-'use strict';
-
-// INITILIZE SERVICE
-// ============================================================
-angular.module('majorGolf').service('gameService', function ($http) {
-
-  // CRUD FUNCTIONS
-  // ============================================================
-  this.getGame = function (id) {
-    // var query = "";
-    // if (id) query = '?_id=' + id;
-    return $http({
-      method: 'GET',
-      url: '/api/games'
-    }).then(function (response) {
-      return response.data;
-    });
-  };
-  this.createGame = function (game) {
-    return $http({
-      method: 'POST',
-      url: '/api/games',
-      data: game
-    }).then(function (response) {
-      return response;
-    });
-  };
-  this.editGame = function (id, game) {
-    return $http({
-      method: 'PUT',
-      url: "/api/games/" + id,
-      data: game
-    }).then(function (response) {
-      return response;
-    });
-  };
-  this.deleteGame = function (id) {
-    return $http({
-      method: 'DELETE',
-      url: '/api/games/' + id
     }).then(function (response) {
       return response;
     });
